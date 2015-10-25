793f1d2f69f30de48cf402e545e49bb50c4ce19e
Lucy Fang <lucyfang2@gmail.com>
2015-10-25 20:44:22 +0000

    amended acceptance tests

--- test/e2e/ToDoListFeature.js
+++ test/e2e/ToDoListFeature.js
@@ -1,12 +1,16 @@
 describe('To Do List', function() {
 
-  var inputBox = element(by.model('listCtrl.newTask'))
-  var checkBox = element(by.model('task.completed'))
-  var addButton = element(by.className('btn'))
+  var inputBox = element(by.model('listCtrl.newTask'));
+  var tasks = element.all(by.repeater('task in listCtrl.list'));
+  var checkBox = $('[type="checkbox"]');
+  var addButton = element(by.id('add-btn'));
+  var allButton = element(by.id('showall-btn'));
+  var activeButton = element(by.id('active-btn'));
+  var completedButton = element(by.id('completed-btn'));
 
   beforeEach(function() {
     browser.get('http://localhost:8080');
-    inputBox.sendKeys('get lunch');
+    inputBox.sendKeys('pay rent');
     addButton.click();
   });
 
@@ -14,10 +18,46 @@ describe('To Do List', function() {
     expect(browser.getTitle()).toEqual('To do list');
   });
 
-  it('displays items added to the list', function() {
-    expect(element(by.binding('task')).getText()).toEqual('get lunch Edit task');
+  it('displays items added to the list and status is incomplete', function() {
+    // expect(tasks.get(0).getText()).toEqual('pay rent');
     expect(inputBox.isSelected()).toBe(false);
+    expect(tasks.count()).toEqual(1);
   });
 
+  it('registers completed status as true when user checks item', function() {
+    checkBox.click();
+    expect((tasks).isDisplayed()).toBeTrue;
+  });
+
+  it('displays count for incomplete items', function() {
+    expect(element(by.className('task-count')).getText()).toEqual('Remaining tasks: 1');
+  });
+
+  it('displays active tasks only', function() {
+    inputBox.sendKeys('call gas company');
+    addButton.click();
+    checkBox.click();
+    activeButton.click();
+    expect(inputBox.isSelected()).toBe(false);
+    expect(tasks.count()).toEqual(1);
+  });
+
+  it('displays completed tasks only', function() {
+    inputBox.sendKeys('call gas company');
+    addButton.click();
+    checkBox.click();
+    completedButton.click();
+    expect((tasks).isDisplayed()).toBeTrue;
+    expect(tasks.count()).toEqual(1);
+  });
+
+  it('displays all tasks', function() {
+    inputBox.sendKeys('call gas company');
+    addButton.click();
+    checkBox.click();
+    activeButton.click();
+    allButton.click();
+    expect(tasks.count()).toEqual(2);
+  });
 
 });
